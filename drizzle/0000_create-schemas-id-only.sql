1776e1b0e2b5dc815c0f09ca727f8eb5c900fd0a
Maroš Pavlík <xpavlik9@fi.muni.cz>
2026-04-19 21:12:09 +0200

    Add initial db migration, remove generate migration from scripts as it gave random names

---
+++ drizzle/0000_create-schemas-id-only.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `dashboard_items` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `groups` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `users` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `widget_data` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `widgets` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL
+);
