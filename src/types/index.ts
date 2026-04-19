5e72953022acd5f5a0e4ac819c894ec22d673e8a
Maroš Pavlík <xpavlik9@fi.muni.cz>
2026-04-19 21:00:57 +0200

    Add re-export of types from src/types for code quality

---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export type { DashboardItem, NewDashboardItem } from '@/lib/db/schemas/dashboard-items';
+export type { Group, NewGroup } from '@/lib/db/schemas/groups';
+export type { User, NewUser } from '@/lib/db/schemas/users';
+export type { WidgetData, NewWidgetData } from '@/lib/db/schemas/widget-data';
+export type { Widget, NewWidget } from '@/lib/db/schemas/widgets';
