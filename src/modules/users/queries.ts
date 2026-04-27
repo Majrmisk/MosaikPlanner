709fd93a71cd695893eb7e36c69d28d8fb94171e
Maroš Pavlík <xpavlik9@fi.muni.cz>
2026-04-27 15:34:49 +0200

    Add user repository and queries, blank actions and components folder

---
+++ src/modules/users/queries.ts
@@ -0,0 +1,19 @@
+'server-only'
+
+import { User } from "./schemas";
+import { getUserById as getUserRecordById } from "./repository";
+
+export const getUserById = async (userId: string): Promise<User | null> => {
+    const user = await getUserRecordById(userId);
+    
+    if (!user) {
+        return null;
+    }
+
+    return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        image: user.image,
+    };
+}
