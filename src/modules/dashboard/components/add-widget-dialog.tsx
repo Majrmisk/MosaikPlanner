5fa4f8c02191b0e2af2cb01f6c6698cca2f8fd9b
Iva Utikalova <iva.utikiv@gmail.com>
2026-05-06 23:50:03 +0200

    Fix prettier formatting

--- src/modules/dashboard/components/add-widget-dialog.tsx
+++ src/modules/dashboard/components/add-widget-dialog.tsx
@@ -156,9 +156,7 @@ export function AddWidgetDialog({
                                                 <SelectItem value="calendar" disabled>
                                                     Calendar (TODO)
                                                 </SelectItem>
-                                                <SelectItem value="checklist">
-                                                    Checklist
-                                                </SelectItem>
+                                                <SelectItem value="checklist">Checklist</SelectItem>
                                                 <SelectItem value="spinner" disabled>
                                                     Spinner (TODO)
                                                 </SelectItem>
