3c4b8ab18be3612fb884db9a846c9c33d94b78b1
LubJur <75535869+LubJur@users.noreply.github.com>
2026-05-08 12:05:03 +0200

    Add basic expenses adding
    # Conflicts:
    #	src/modules/dashboard/components/add-widget-dialog.tsx
    #	src/modules/widgets/components/widget-editors.ts

--- src/modules/widgets/components/widget-editors.ts
+++ src/modules/widgets/components/widget-editors.ts
@@ -2,6 +2,7 @@ import type { ComponentType } from 'react';
 import type { WidgetType } from '@/lib/db/schemas/widgets';
 import type { WidgetEditorProps } from './widget-editor-props';
 import { NotesEditor } from './notes-editor';
+import { ExpensesEditor } from "@/modules/widgets/components/expenses";
 import { CalendarEditor } from './calendar-editor';
 import { ChecklistEditor } from './checklist-editor';
 import { SpinnerEditor } from './spinner-editor';
@@ -11,5 +12,5 @@ export const widgetEditors: Partial<Record<WidgetType, ComponentType<WidgetEdito
     calendar: CalendarEditor,
     checklist: ChecklistEditor,
     spinner: SpinnerEditor,
-    // TODO expenses: ExpensesEditor,
+    expenses: ExpensesEditor,
 };
